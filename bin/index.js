5c98cf4a51ee7f12640ec8c2b7ad0b59968b9db0
riswanda21 <albertcastell12@gmail.com>
2022-12-21 12:01:43 +0800

    Update

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs/yargs')
+const figlet = require("figlet")
 const {
   hideBin
 } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
-const server = require("./server.js");
-const create = require("./create.js");
+const server = require("./server.js")
+const create = require("./create.js")
 
+figlet.text("M e l l o w J S", function (err, data){
+  console.log('------------------------------------------------------------\n' + data + '\n------------------------------------------------------------\n')
+ })
 if (argv.server != null) {
   server.main();
 }
