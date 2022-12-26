a9b8ea75b826c974074be9ff80c9bccf5d589f3b
riswanda21 <albertcastell12@gmail.com>
2022-12-26 14:49:11 +0800

    Create build settings

--- bin/create.js
+++ bin/create.js
@@ -2,7 +2,7 @@
 
 const yargs = require('yargs/yargs')
 const {
-	hideBin
+  hideBin
 } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
 const fs = require("fs");
@@ -12,34 +12,39 @@ const path = argv.create;
 ncp.limit = 16;
 
 exports.main = function() {
-	var project_dir = paths.join(paths.dirname(fs.realpathSync(__filename)), '../');
-	fs.mkdir(process.cwd() + '\\' + argv.create, (error) => {
-		if (error) {
-			console.log(error);
-		} else {
-			fs.mkdir(process.cwd() + '\\' + argv.create + '\\src', (error) => {
-				if (error) {
-					console.log(error);
-				} else {
-					fs.writeFile(argv.create + "\\android.json", `{
-              "applicationId": "com.example.app"
+  var project_dir = paths.join(paths.dirname(fs.realpathSync(__filename)), '../');
+  fs.mkdir(process.cwd() + '\\' + argv.create, (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      fs.mkdir(process.cwd() + '\\' + argv.create + '\\src', (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          fs.writeFile(argv.create + "\\android.json", `{
+              "applicationId": "com.example.app",
+              "appName": "Example Project",
+			  "versionCode": "1.0",
+			  "versionName": "1.0",
+			  "minSdkVersion": "23.0",
+			  "targetSdkVersion": "33.0"
 }`, (err) => {
-						if (err) console.log(err);
-						console.log("[+] Created Android Settings File.");
-					});
-					fs.writeFile(argv.create + "\\windows.json", "{}", (err) => {
-						if (err) console.log(err);
-						console.log("[+] Created Windows Settings File.");
-					});
-					ncp(project_dir + '\\src', argv.create + '\\src',
-						function(err) {
-							if (err) {
-								return console.error(err);
-							}
-							console.log('[+] Successfully create new project.');
-						});
-				}
-			});
-		}
-	});
+            if (err) console.log(err);
+            console.log("[+] Created Android Settings File.");
+          });
+          fs.writeFile(argv.create + "\\windows.json", "{}", (err) => {
+            if (err) console.log(err);
+            console.log("[+] Created Windows Settings File.");
+          });
+          ncp(project_dir + '\\src', argv.create + '\\src',
+            function(err) {
+              if (err) {
+                return console.error(err);
+              }
+              console.log('[+] Successfully create new project.');
+            });
+        }
+      });
+    }
+  });
 }
