b8bc8da33fde10d0d34fdf31219fe82f9d317519
riswanda21 <albertcastell12@gmail.com>
2022-12-29 22:10:35 +0800

    Update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import Mellow from './mellow/core.js'
 import Home from './views/home.js'
 
 const Main = function() {
-  Mellow.route('/', Home)
+    Mellow.route('/', Home)
 }
-
-Mellow.ready(Main)ellow)
+Mellow.ready(Main)llow)
