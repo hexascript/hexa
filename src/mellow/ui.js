73c581c47ea9a05828dac260964e4d23ad482ea9
rdcreativestudio.id@gmail.com <rdcreativestudio.id@gmail.com>
2022-12-31 10:23:04 +0800

    Update UI

--- src/mellow/ui.js
+++ src/mellow/ui.js
@@ -12,22 +12,17 @@ export function AppBar(data) {
          </div>`
 }
 
-export function Alert(data) {
+export function Alerts(data) {
   return `<div id="${data.id}" style="display:${data.display};" class="bg-${data.backgroundColor} border border-${data.borderColor} text-${data.textColor} px-4 py-3 rounded relative ${data.class}" role="alert">
-       <span class="block sm:inline">${data[0].text}</span></div>`
+       <span class="block sm:inline">${data.text}</span></div>`
 }
 
-export function Input(data) {
-  if(data.function == null) {
-       var functs = "";
-  } else {
-       var functs = data.function;
-  }
-  if(data.return == null) {
-       var rets = "";
-  } else {
-       var rets = `x-model="${data.return}"`
+export function TextBox(data) {
+  if(data.readOnly == "true") {
+       var readonly = "readonly"
+  }else{
+       var readonly = ""
   }
   return `<label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">${data.labelText}</label>
-        <input type="${data.type}" id="${data.id}" {$rets} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="${data.placeHolder}" ${functs}>`
+        <input type="${data.type}" id="${data.id}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="${data.placeHolder}" ${data.readonly}>`
 }
