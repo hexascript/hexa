ae67832ed3d8430822981d17a52f235b09c6d38e
riswanda21 <riswanda21>
2023-01-01 20:04:41 +0800

    Update

--- src/hexa/ui.js
+++ src/hexa/ui.js
@@ -1,3 +1,5 @@
+import '../public/js/tailwind.js'
+
 export function AppBar(data) {
   if (data.backButton == 'true') {
        var backbutton = `<div onclick="history.back()" class="hover:text-slate-500"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="15 18 9 12 15 6"></polyline></svg></div>`
