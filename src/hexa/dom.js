3caffdde0c23341e093f63b3dffb1c24eed49fc0
riswanda21 <riswanda21>
2023-01-04 01:15:43 +0800

    Update to ES6+

--- src/hexa/dom.js
+++ src/hexa/dom.js
@@ -1,26 +1,26 @@
 const HexaDOM = {
 
     element(id) {
-      if (id.charAt(0) == '#') {
+      if(id.startsWith('#') == true) {
         return document.getElementById(id.replace('#', ""))
-      } else if (id.charAt(0) == '.') {
+      } else if(id.startsWith('.') == true) {
         return document.getElementsByClassName(id.replace('.', ""))
       }
     },
     
     addEvent(event, id, func) {
-        if (id.charAt(0) == '#') {
+        if(id.startsWith('#') == true) {
           var element = document.getElementById(id.replace('#', ""))
-        } else if (id.charAt(0) == '.') {
+        } else if(id.startsWith('.') == true) {
           var element = document.getElementsByClassName(id.replace('.', ""))
         }
         element.addEventListener(event, func)
     },
     
     render(id, data) {
-      if(id.charAt(0) == '#') {
+      if(id.startsWith('#') == true) {
         var template = document.getElementById(id.replace('#', ""))
-      }else if(id.charAt(0) == '.') {
+      }else if(id.startsWith('.') == true) {
         var template = document.getElementsByClassName(id.replace('.', ""))
       }
       const result = data.replace(/<view>(.*?)<\/view>/gis, '<div class="mx-auto"><div class="h-full bg-gray-50">$1</div></div>')
