2f77e56cdd07a09f869546aebd064a3d23ed0335
riswanda21 <riswanda21>
2023-01-08 15:02:11 +0800

    Update HexaDOM element

--- src/hexa/dom.js
+++ src/hexa/dom.js
@@ -1,5 +1,4 @@
 const HexaDOM = {
-
     element(id) {
       if(id.startsWith('#') == true) {
         return document.getElementById(id.replace('#', ""))
@@ -7,28 +6,17 @@ const HexaDOM = {
         return document.getElementsByClassName(id.replace('.', ""))
       }
     },
-    
     addEvent(event, id, func) {
-        if(id.startsWith('#') == true) {
-          var element = document.getElementById(id.replace('#', ""))
-        } else if(id.startsWith('.') == true) {
-          var element = document.getElementsByClassName(id.replace('.', ""))
-        }
+        const element = this.element(id)
         element.addEventListener(event, func)
     },
-    
     render(id, data) {
-      if(id.startsWith('#') == true) {
-        var template = document.getElementById(id.replace('#', ""))
-      }else if(id.startsWith('.') == true) {
-        var template = document.getElementsByClassName(id.replace('.', ""))
-      }
-      const result = data.replace(/<view>(.*?)<\/view>/gis, '<div class="mx-auto"><div class="h-full bg-gray-50">$1</div></div>')
+        const template = this.element(id)
+        const result = data.replace(/<view>(.*?)<\/view>/gis, '<div class="mx-auto"><div class="h-full bg-gray-50">$1</div></div>')
         .replace(/<content>(.*?)<\/content>/gis, '<div class="p-6">$1</div>')
         
-      template.innerHTML = result
+        template.innerHTML = result
     }
-    
 }
 
 export default HexaDOM
